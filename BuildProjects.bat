8fb5ca652f55771d7bdf3c268a18e944000074fb
SemperParatusGithub <73690312+SemperParatusGithub@users.noreply.github.com>
2020-12-04 21:56:33 +0100

    Delete BuildProjects.bat

--- BuildProjects.bat
+++
@@ -1,5 +0,0 @@
-set msBuildDir=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin
-cd build
-"%msBuildDir%\MSBuild.exe" /p:Platform=x64 /p:Configuration=Release /p:DisableFastUpToDateCheck=1 -m Calculator.sln
-
-xcopy /e /v Calculator\res bin\Release-windows-x86_64\Calculator\res /i
