00b525dfef64be46ef27c9ba9b9a93516d728c47
SemperParatusGithub <schlagernils@gmail.com>
2020-12-13 19:50:01 +0100

    Added .f to all values

--- Calculator/src/MenuItems/Vectors.cpp
+++ Calculator/src/MenuItems/Vectors.cpp
@@ -16,9 +16,9 @@ Vectors::~Vectors()
 
 void Vectors::OnContentRender()
 {
-	ImVec2 buttonSize = ImVec2 { m_WindowSize.x / 35, 0.0f };
+	ImVec2 buttonSize = ImVec2 { m_WindowSize.x / 35.0f, 0.0f };
 
-	ImGui::Text(" Vector 1: ");  ImGui::SameLine(m_WindowSize.x / 3.5);
+	ImGui::Text(" Vector 1: ");  ImGui::SameLine(m_WindowSize.x / 3.5f);
 	ImGui::Text(" Vector 2: ");
 
 	ImGui::SetNextItemWidth(m_WindowSize.x / 4); ImGui::DragFloat("##vec11", &vec1[0], 0.1f, 0.0f, 0.0f, "%.3f"); ImGui::SameLine(m_WindowSize.x / 4);
@@ -29,9 +29,9 @@ void Vectors::OnContentRender()
 	if (ImGui::Button("x##1", buttonSize)) vec1[0] = 0;
 	ImGui::PopFont();
 	ImGui::PopStyleColor(3);
-	ImGui::SameLine(m_WindowSize.x / 3.5);
+	ImGui::SameLine(m_WindowSize.x / 3.5f);
 
-	ImGui::SetNextItemWidth(m_WindowSize.x / 4); ImGui::DragFloat("##vec21", &vec2[0], 0.1f, 0.0f, 0.0f, "%.3f"); ImGui::SameLine(m_WindowSize.x / 1.867);
+	ImGui::SetNextItemWidth(m_WindowSize.x / 4.0f); ImGui::DragFloat("##vec21", &vec2[0], 0.1f, 0.0f, 0.0f, "%.3f"); ImGui::SameLine(m_WindowSize.x / 1.867f);
 	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.8f, 0.1f, 0.15f, 1.0f });
 	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.9f, 0.2f, 0.2f, 1.0f });
 	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.8f, 0.1f, 0.15f, 1.0f });
@@ -40,7 +40,7 @@ void Vectors::OnContentRender()
 	ImGui::PopFont();
 	ImGui::PopStyleColor(3);
 
-	ImGui::SetNextItemWidth(m_WindowSize.x / 4); ImGui::DragFloat("##vec12", &vec1[1], 0.1f, 0.0f, 0.0f, "%.3f"); ImGui::SameLine(m_WindowSize.x / 4);
+	ImGui::SetNextItemWidth(m_WindowSize.x / 4.0f); ImGui::DragFloat("##vec12", &vec1[1], 0.1f, 0.0f, 0.0f, "%.3f"); ImGui::SameLine(m_WindowSize.x / 4.0f);
 	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.f, 0.4f, 0.05f, 1.0f });
 	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.1f, 0.5f, 0.15f, 1.0f });
 	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.f, 0.4f, 0.05f, 1.0f });
@@ -48,9 +48,9 @@ void Vectors::OnContentRender()
 	if (ImGui::Button("y##1", buttonSize))  vec1[1] = 0;
 	ImGui::PopFont();
 	ImGui::PopStyleColor(3);
-	ImGui::SameLine(m_WindowSize.x / 3.5);
+	ImGui::SameLine(m_WindowSize.x / 3.5f);
 
-	ImGui::SetNextItemWidth(m_WindowSize.x / 4); ImGui::DragFloat("##vec22", &vec2[1], 0.1f, 0.0f, 0.0f, "%.3f"); ImGui::SameLine(m_WindowSize.x / 1.867);
+	ImGui::SetNextItemWidth(m_WindowSize.x / 4.0f); ImGui::DragFloat("##vec22", &vec2[1], 0.1f, 0.0f, 0.0f, "%.3f"); ImGui::SameLine(m_WindowSize.x / 1.867f);
 	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.f, 0.4f, 0.05f, 1.0f });
 	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.1f, 0.5f, 0.15f, 1.0f });
 	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.f, 0.4f, 0.05f, 1.0f });
@@ -59,7 +59,7 @@ void Vectors::OnContentRender()
 	ImGui::PopFont();
 	ImGui::PopStyleColor(3);
 
-	ImGui::SetNextItemWidth(m_WindowSize.x / 4); ImGui::DragFloat("##vec13", &vec1[2], 0.1f, 0.0f, 0.0f, "%.3f"); ImGui::SameLine(m_WindowSize.x / 4);
+	ImGui::SetNextItemWidth(m_WindowSize.x / 4.0f); ImGui::DragFloat("##vec13", &vec1[2], 0.1f, 0.0f, 0.0f, "%.3f"); ImGui::SameLine(m_WindowSize.x / 4.0f);
 	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.1f, 0.25f, 0.8f, 1.0f });
 	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.2f, 0.35f, 0.9f, 1.0f });
 	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.1f, 0.25f, 0.8f, 1.0f });
@@ -67,9 +67,9 @@ void Vectors::OnContentRender()
 	if (ImGui::Button("z##1", buttonSize)) vec1[2] = 0;
 	ImGui::PopFont();
 	ImGui::PopStyleColor(3);
-	ImGui::SameLine(m_WindowSize.x / 3.5);
+	ImGui::SameLine(m_WindowSize.x / 3.5f);
 
-	ImGui::SetNextItemWidth(m_WindowSize.x / 4); ImGui::DragFloat("##vec23", &vec2[2], 0.1f, 0.0f, 0.0f, "%.3f"); ImGui::SameLine(m_WindowSize.x / 1.867);
+	ImGui::SetNextItemWidth(m_WindowSize.x / 4.0f); ImGui::DragFloat("##vec23", &vec2[2], 0.1f, 0.0f, 0.0f, "%.3f"); ImGui::SameLine(m_WindowSize.x / 1.867f);
 	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4 { 0.1f, 0.25f, 0.8f, 1.0f });
 	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4 { 0.2f, 0.35f, 0.9f, 1.0f });
 	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4 { 0.1f, 0.25f, 0.8f, 1.0f });
